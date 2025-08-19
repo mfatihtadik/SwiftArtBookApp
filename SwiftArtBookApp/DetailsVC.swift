47c6021bef5a109ffb8978a5899c897c8afe5f52
Muhammed Fatih Tadık <fatihtadk@gmail.com>
2025-08-19 21:39:38 +0300

    Design were created.

---
+++ SwiftArtBookApp/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  SwiftArtBookApp
+//
+//  Created by Fatih Tadık on 19.08.2025.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+    
+    @IBOutlet weak var myImageview: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+
+}
